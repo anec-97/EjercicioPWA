956cd5e2429ed2202fa286bbc2a5fea1427ab713
anec_97 <anechdz22@gmail.com>
2024-02-08 17:53:34 -0600

    primer commit

---
+++ main.js
@@ -0,0 +1,13 @@
+//cargar Service Worker
+if('serviceWorker' in navigator)
+{
+    console.log("Puedes usar el Service Worker");
+    //configuración del SW
+    navigator.serviceWorker.register('./sw.js')
+                            .then(res=>console.log('SW cargado correctamente',res))
+                            .catch(err => console.log('service Worker no se ha podido registrar',err));
+}
+else 
+{
+    console.log("No puedes usar el Service Worker");
+}
